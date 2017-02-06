bde6f2edf137d22d49b40fc3d614da1143055267
Maddie <mk186442@falmouth.ac.uk>
2017-02-06 14:21:42 +0000

    Commented Character.java

--- Character.java
+++ Character.java
@@ -1,20 +1,29 @@
 
 public class Character {
 
+	// Character X location
 	private int x = 0;
+	// Character Y location
 	private int y = 0;
+	// Whether the character is active
 	private boolean isActive = true;
 	
+	// Sets character x and y on initialisation
 	public Character (int initialX, int initialY){
 		setX(initialX);
 		setY(initialY);
 	}
-	
+	//Getters
 	public int getX(){return x;}
 	public int getY(){return y;}
-	
+	// Setters
 	public void setX(int newX){x = newX;}
 	public void setY(int newY){y = newY;}
-	
-	
+
+	// Get Cell that the character is on
+	// Only works within regions
+	public Cell getCell(Chunk mapChunk)
+	{
+		return mapChunk.getCells()[x][y];
+	}
 }

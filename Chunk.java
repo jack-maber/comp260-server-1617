dac21b80dd0aee5a0ee56c9d9e6d8a88dc21b26b
WarwickNew <warwick.l.e.new@gmail.com>
2017-02-06 13:16:21 +0000

    fixed get cells in chunk class

--- Chunk.java
+++ Chunk.java
@@ -1,6 +1,15 @@
 
 public class Chunk {
+	// smallest area to be loaded at once for usability reasons
 
+	public Chunk(){
+		for (int x = 0; x < chunk.length; x++){
+			for (int y = 0; y < chunk[0].length; y++){
+				chunk[x][y] = new Cell();
+			}
+		}
+	}
+	
 	private Cell[][] chunk = new Cell[5][5];
 	
 	public Cell[][] getCells() { return chunk; };
--- Region.java
+++ Region.java
@@ -1,6 +1,8 @@
 
 public class Region {
-
+	/* contains multiple chunks preferably the use of multiple
+	 * regions should be for large numbers of people
+	*/
 	private Chunk[][] region = new Chunk[5][5];
 	
 	public Chunk[][] getChunks() {return region;}

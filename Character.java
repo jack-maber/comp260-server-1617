4d10b3b322748ef73bad317708add3da14a6fc07
teasterbrook17 <tseasterbrook17@gmail.com>
2017-02-06 13:31:59 +0000

    added character class - tom and maddie

---
+++ Character.java
@@ -0,0 +1,19 @@
+
+public class Character {
+
+	private int x = 0;
+	private int y = 0;
+	
+	public Character (int initialX, int initialY){
+		setX(initialX);
+		setY(initialY);
+	}
+	
+	public int getX(){return x;}
+	public int getY(){return y;}
+	
+	public void setX(int newX){x = newX;}
+	public void setY(int newY){y = newY;}
+	
+	
+}

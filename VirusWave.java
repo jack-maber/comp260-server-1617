c6e875eeb23b61dcd32ddb699c4b27768efd022b
teasterbrook17 <tseasterbrook17@gmail.com>
2017-02-13 15:01:19 +0000

    updated break timer

--- VirusWave.java
+++ VirusWave.java
@@ -1,57 +1,32 @@
-//To do:
-//when player starts wave it starts the wave length timer
-//when player kills wave it starts break timer
-//when break timer finishes it starts wave timer
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
- 
-public class VirusWave extends TimerTask {
+public class VirusWave {
+static int interval;
+static Timer timer;
 
-	
-	@Override
-	public void run(){
-		System.out.println("Wave Started");
-		completeWave();
-		System.out.println("Wave Ended");
-	}
-	
-	
-	private void completeWave(){
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e){
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public static void main(String arg[]){
-		TimerTask waveTimer = new VirusWave();
-		
-		Timer timer = new Timer(true);
-		timer.scheduleAtFixedRate(waveTimer, 0, 10*500);
-		System.out.println("Wave Started");
-		try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Break begins ");
+    int secs = 30;
+    int delay = 500;
+    int period = 500;
+    timer = new Timer();
+    interval = 30;
+    System.out.println(secs);
+    timer.scheduleAtFixedRate(new TimerTask() {
+
+        public void run() {
+            System.out.println(setInterval());
+
         }
-        timer.cancel();
-        System.out.println("Wave cancelled");
-        try {
-            Thread.sleep(30000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-		
-		
-		
-	}
-	
-	
-	
-	
-	
+    }, delay, period);
 }
 
+private static final int setInterval() {
+    if (interval == 1)
+        timer.cancel();
+    return --interval;
+}
+}

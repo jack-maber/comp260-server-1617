d8c57500ad2ed19aa3b9c8f29a50def9df4c1f7d
WarwickNew <warwick.l.e.new@gmail.com>
2017-02-17 15:15:36 +0000

    fixing last commit

---
+++ Timer.java
@@ -0,0 +1,40 @@
+// singleton timer
+
+public class Timer {
+
+	private static Timer timer = new Timer();
+
+	private Thread thread;
+
+	private Timer() {
+		thread = new TickThread();
+		thread.start();
+	}
+
+	public static Timer getInstance() {
+		return timer;
+	}
+
+	protected Thread getTick() {
+		return thread;
+	}
+
+	protected void killTimer() {
+		thread.interrupt();
+	}
+}
+
+class TickThread extends Thread {
+	// waits for a specified time and then will go through all the commands.
+	public void run() {
+		while (true) {
+			System.out.println("tick");
+			try {
+				Thread.sleep(200);
+			}
+			catch (InterruptedException threadShutdown) {
+				break;
+			}
+		}
+	}
+}

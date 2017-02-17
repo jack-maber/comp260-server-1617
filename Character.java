7aeeade52d25910fc2144cd71c612d2809e0bc41
Maddie <mk186442@falmouth.ac.uk>
2017-02-17 10:25:16 +0000

    Added reference to Map in Character

--- Character.java
+++ Character.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Character {
 
 	// Character X location
@@ -5,15 +7,21 @@ public class Character {
 	// Character Y location
 	private int y = 0;
 	
-	// Stores which player this character is atteched to.
+	// Stores which player this character is attached to.
 	private int playerID;
 	
 	private Cell currentCell;
+	
+	HashMap movementCommands = new HashMap(); 
+	
+	Map map = Map.getInstance();
 
 	// Sets character x and y on initialisation
 	public Character(int initialX, int initialY, int playerID) {
 		setPosition(initialX, initialY);
 		this.playerID = playerID;
+		movementCommands.put("north", 1);
+		movementCommands.put("south", -1);
 	}
 
 	// Getters
@@ -30,19 +38,33 @@ public class Character {
 		x = newX;
 		y = newY;
 	}
-
+/*
 	public void getCurrentCell(Map map){
 		currentCell = map.getCells()[x][y];
 	}
 	
-	public synchronized void moveCharacter(int moveToX, int moveToY, Map map){
-		if (map.getCells()[moveToX][moveToY].getCellContent() == null && (map.getCells()[moveToX][moveToY].cellTaken))
+	public boolean checkCell(Map map,int moveToX, int moveToY){
+		if (map.getCells()[moveToX][moveToY].getCellContent() == null 
+				&& (map.getCells()[moveToX][moveToY].cellTaken))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	
+	
+	public synchronized void moveCharacter(String inputMovement, Map map){
+		if (checkCell(map))
 		{
 			setPosition(moveToX, moveToY);
 			currentCell.cellTaken = false;
 			currentCell = map.getCells()[x][y];
 			currentCell.cellTaken = true;
 		}
-	}
+	}*/
 	
 }

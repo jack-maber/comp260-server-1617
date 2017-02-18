2078e16fc9a37e225e4eec5f28e6729f3148c97d
teasterbrook17 <tseasterbrook17@gmail.com>
2017-02-18 20:25:47 +0000

    adding synchronized to make sure all threads read from the timer at the same time so they all have the same time

--- BreakWave.java
+++ BreakWave.java
@@ -3,11 +3,16 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class BreakWave {
-
+	
 	static int interval;
 	static Timer timer;
 
 	public synchronized static void main(String[] args) {
+		
+		boolean round = false;  //The round is false which means that the break time countdown starts
+		
+		while (round = false);
+		{
 	    Scanner sc = new Scanner(System.in);
 	    System.out.println("Break has began ");
 	    int secs = 30;
@@ -23,12 +28,14 @@ public class BreakWave {
 
 	        }
 	    }, delay, period);
-	}
+	}}
 
-	private static final int setInterval() {
+	private synchronized static final int setInterval() { //synchronized is used to keep the reading of this for all threads consistent so they follow the same time
 	    if (interval == 1)
 	        timer.cancel();
+	    	boolean round = true; //if the interval is finished sets the round value to true which will start the enemy wave
 	    return --interval;
 	}
+	
 	
 }

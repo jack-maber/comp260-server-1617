e7319837632eaa5b53fd5e251a714d7308716fce
jack-maber <jack-maber@live.co.uk>
2018-02-10 13:05:43 +0000

    Added server

---
+++ Client/Client/client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    class client
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ipLocal = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8221);
+
+			bool connected = false;
+
+			while (connected == false) 
+			{
+				try 
+				{
+					s.Connect (ipLocal);
+					connected = true;
+				} 
+				catch (Exception) 
+				{
+					Thread.Sleep (1000);
+				}
+			}
+
+            int ID = 0;
+
+            while (true)
+            {
+                String Msg = ID.ToString() + " testing, testing, 1,2,3";
+                ID++;
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                byte[] buffer = encoder.GetBytes(Msg);
+
+                try
+                {
+                    Console.WriteLine("Writing to server: " + Msg);
+                    int bytesSent = s.Send(buffer);
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine(ex);	
+                }
+                
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Server/Server/server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class server
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ipLocal = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8221);
+			
+            s.Bind(ipLocal);
+            s.Listen(4);
+
+            Console.WriteLine("Waiting for client ...");
+            
+            Socket newConnection = s.Accept();
+            if (newConnection != null)
+            {            
+                while (true)
+                {
+                    byte[] buffer = new byte[4096];
+
+                    try
+                    {
+                        int result = newConnection.Receive(buffer);
+
+                        if (result > 0)
+                        {
+                            ASCIIEncoding encoder = new ASCIIEncoding();
+                            String recdMsg = encoder.GetString(buffer, 0, result);
+
+                            Console.WriteLine("Received: " + recdMsg);
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine(ex);    	
+                    }                    
+                }
+            }
+        }
+    }
+}

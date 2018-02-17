d45470452da16648c207752f215bde85ed4b476a
jack-maber <jack-maber@live.co.uk>
2018-02-17 19:28:34 +0000

    Added chat systems

---
+++ 03.Chat system/Server/code/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+using MessageTypes;
+
+
+namespace Server
+{
+    class Program
+    {
+        static Dictionary<String,Socket> clientDictionary = new Dictionary<String,Socket>();
+        static int clientID = 1;
+
+        static void SendClientName(Socket s, String clientName)
+        {
+            ClientNameMsg nameMsg = new ClientNameMsg();
+            nameMsg.name = clientName;
+
+            MemoryStream outStream = nameMsg.WriteData();
+
+            s.Send(outStream.GetBuffer() );
+        }
+
+        static void SendClientList()
+        {
+            ClientListMsg clientListMsg = new ClientListMsg();
+
+            lock (clientDictionary)
+            {
+                foreach (KeyValuePair<String, Socket> s in clientDictionary)
+                {
+                    clientListMsg.clientList.Add(s.Key);
+                }
+
+                MemoryStream outStream = clientListMsg.WriteData();
+
+                foreach (KeyValuePair<String, Socket> s in clientDictionary)
+                {
+                    s.Value.Send(outStream.GetBuffer());
+                }
+            }
+        }
+
+        static void SendChatMessage(String msg)
+        {
+            PublicChatMsg chatMsg = new PublicChatMsg();
+
+            chatMsg.msg = msg;
+
+            MemoryStream outStream = chatMsg.WriteData();
+
+            lock (clientDictionary)
+            {            
+                foreach (KeyValuePair<String,Socket> s in clientDictionary)
+                {
+                    try
+                    {
+                        s.Value.Send(outStream.GetBuffer());
+                    }
+                    catch (System.Exception)
+                    {
+                    	
+                    }                    
+                }
+            }
+        }
+
+        static void SendPrivateMessage(Socket s, String from, String msg)
+        {
+            PrivateChatMsg chatMsg = new PrivateChatMsg();
+            chatMsg.msg = msg;
+            chatMsg.destination = from;
+            MemoryStream outStream = chatMsg.WriteData();
+
+            try
+            {
+                s.Send(outStream.GetBuffer());
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+
+        static Socket GetSocketFromName(String name)
+        {
+            lock (clientDictionary)
+            {
+                return clientDictionary[name];
+            }
+        }
+
+        static String GetNameFromSocket(Socket s)
+        {
+            lock (clientDictionary)
+            {
+                foreach (KeyValuePair<String, Socket> o in clientDictionary)
+                {
+                    if (o.Value == s)
+                    {
+                        return o.Key;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        static void RemoveClientBySocket(Socket s)
+        {
+            string name = GetNameFromSocket(s);
+
+            if (name != null)
+            {
+                lock (clientDictionary)
+                {
+                    clientDictionary.Remove(name);
+                }
+            }
+        }
+
+
+        static void receiveClientProcess(Object o)
+        {
+            bool bQuit = false;
+
+            Socket chatClient = (Socket)o;
+
+            Console.WriteLine("client receive thread for " + GetNameFromSocket(chatClient));
+
+            SendClientList();
+
+            while (bQuit == false)
+            {
+                try
+                {
+                    byte[] buffer = new byte[4096];
+                    int result;
+
+                    result = chatClient.Receive(buffer);
+
+                    if (result > 0)
+                    {
+                        MemoryStream stream = new MemoryStream(buffer);
+                        BinaryReader read = new BinaryReader(stream);
+
+                        Msg m = Msg.DecodeStream(read);
+
+                        if (m != null)
+                        {
+                            Console.Write("Got a message: ");
+                            switch (m.mID)
+                            {
+                                case PublicChatMsg.ID:
+                                    {
+                                        PublicChatMsg publicMsg = (PublicChatMsg)m;
+
+                                        String formattedMsg = "<" + GetNameFromSocket(chatClient)+"> " + publicMsg.msg;
+
+                                        Console.WriteLine("public chat - " + formattedMsg);
+
+                                        SendChatMessage(formattedMsg);
+                                    }
+                                    break;
+
+                                case PrivateChatMsg.ID:
+                                    {
+                                        PrivateChatMsg privateMsg = (PrivateChatMsg)m;
+
+                                        String formattedMsg = "PRIVATE <" + GetNameFromSocket(chatClient) + "> " + privateMsg.msg;
+
+                                        Console.WriteLine("private chat - " + formattedMsg + "to " + privateMsg.destination);
+
+                                        SendPrivateMessage(GetSocketFromName(privateMsg.destination), GetNameFromSocket(chatClient), formattedMsg);
+
+                                        formattedMsg = "<" + GetNameFromSocket(chatClient) + "> --> <" +privateMsg.destination+"> " + privateMsg.msg;
+                                        SendPrivateMessage(chatClient, "", formattedMsg);
+                                    }
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
+                    }                   
+                }
+                catch (Exception)
+                {
+                    bQuit = true;
+
+                    String output = "Lost client: " + GetNameFromSocket(chatClient);
+                    Console.WriteLine(output);
+                    SendChatMessage(output);
+
+                    RemoveClientBySocket(chatClient);
+
+                    SendClientList();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8500));
+            serverSocket.Listen(32);
+
+            bool bQuit = false;
+
+            Console.WriteLine("Server");
+
+            while (!bQuit)
+            {
+                Socket serverClient = serverSocket.Accept();
+
+                Thread myThread = new Thread(receiveClientProcess);
+                myThread.Start(serverClient);
+
+                lock (clientDictionary)
+                {
+                    String clientName = "client" + clientID;
+                    clientDictionary.Add(clientName, serverClient);
+
+                    SendClientName(serverClient, clientName);
+                    Thread.Sleep(500);
+                    SendClientList();
+
+                    clientID++;
+                }
+                
+            }
+        }
+    }
+}
